a50abc0b3ac283bd3ab480560fef33db0c6328ce
pyVarad <22f3001152@ds.study.iitm.ac.in>
2025-06-14 16:56:35 +0530

    baseline_2

--- packages/codegen-plugin/src/extension.ts
+++ packages/codegen-plugin/src/extension.ts
@@ -15,7 +15,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
-    "codegen-plugin.helloWorld",
+    "codegen-plugin.reactgenerator",
     () => {
       // Create and show a new webview panel
       const panel = vscode.window.createWebviewPanel(
